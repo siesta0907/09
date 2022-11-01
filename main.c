3d5bdb2a960a1d5c94e2007c37d60b8b7fbddab2
siesta0907 <50662414+siesta0907@users.noreply.github.com>
2022-11-02 03:09:22 +0900

    09-04

--- main.c
+++ main.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
-#define SIZE 5
-
+#define SIZE 4
+void square_array(int a[], int size);
+void print_array(int a[], int size);
 int main(void)
 {
+	int list[SIZE] = {1, 2, 3, 4};
+	print_array(list, SIZE);
+	square_array(list, SIZE);
+	print_array(list, SIZE);
+	return 0;
+}
+void square_array(int a[], int size)
+{
 	int i;
-	int a[SIZE] = {1, 2, 3, 4, 5};
-	int b[SIZE] = {1, 2, 9, 4, 5};
-	int flag_same = 1;
-	for (i=0; i<SIZE; i++)
-	{
-		if (a[i] != b[i])
-		{
-			printf("%d 번째 요소는 서로 다릅니다", i);
-			flag_same = 0;
-		}
-	}
-	if (flag_same) printf("두 배열은 같습니다.");
+	for (i=0; i<size; i++)
+	a[i] = a[i] * a[i];
+}
+void print_array(int a[], int size)
+{
+	int i;
+	for (i=0; i<size; i++)
+	printf("%3d", a[i]);
+	printf("\n");
 }
